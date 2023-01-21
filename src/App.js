e84abee96c27dfb3ac8cea9cc0bf4d5d96cc887b
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-01-21 20:37:14 +0300

    created google-translate project file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import TextBox from "./components/text-box/TextBox";
+import Arrows from "./components/arrows/Arrows";
+import Button from "./components/button/Button";
+import Modal from "./components/modal/Modal";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
