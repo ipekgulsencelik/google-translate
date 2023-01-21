e556c9e18a9483399204187d9a0f518946ccdec2
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-01-21 21:27:20 +0300

    added select drop-down

--- src/components/drop-down/DropDown.js
+++ src/components/drop-down/DropDown.js
@@ -1,7 +1,21 @@
+import "./style.css";
+
 const DropDown = () => {
-    return (
-        <div>DropDown</div>
-    )
-}
+  return (
+    <div className="select-drop-down">
+      <input />
+
+      <div className="down-arrow">
+        <svg
+          focusable="false"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 25"
+        >
+          <path d="M7 10l5 5 5-5z"></path>
+        </svg>
+      </div>
+    </div>
+  );
+};
 
 export default DropDown;
