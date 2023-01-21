3a00f3d1d37c875b8f060b1a4a0052b31edaead9
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-01-21 22:05:59 +0300

    added control to show modal

--- src/App.js
+++ src/App.js
@@ -18,19 +18,24 @@ const App = () => {
 
   return (
     <div className="App">
-      <TextBox
-        selectedLanguage={inputLanguage}
-        style="input"
-        setShowModal={setShowModal}
-      />
-      <div className="arrow-container" onClick={handleClick}>
-        <Arrows />
-      </div>
-      <TextBox
-        selectedLanguage={outputLanguage}
-        style="output"
-        setShowModal={setShowModal}
-      />
+      {!showModal && (
+        <>
+          <TextBox
+            selectedLanguage={inputLanguage}
+            style="input"
+            setShowModal={setShowModal}
+          />
+          <div className="arrow-container" onClick={handleClick}>
+            <Arrows />
+          </div>
+          <TextBox
+            selectedLanguage={outputLanguage}
+            style="output"
+            setShowModal={setShowModal}
+          />
+        </>
+      )}
+      {showModal && <Modal />}
     </div>
   );
 };
